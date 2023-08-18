bfd4fa640e5ccee765952ff53e8d2a2f4bf593b8
Sergio Oliveira <67247374+sergiordob@users.noreply.github.com>
2023-08-18 18:30:05 -0300

    task 01 ok!

---
+++ BootCamps/BootCamp_Santander_Java_DIO/Tasks/Task01/ContaBanco/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal
+{
+    public static void main (String[] args)
+    {
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
+
+        int numero = 0;
+        double saldo = 0.0;
+        String agencia = "";
+        String nomeDoCliente = "";
+
+        System.out.print("Digite o nome do cliente: ");
+        nomeDoCliente = entrada.nextLine();
+        System.out.print("Digite o numero da Agencia: ");
+        agencia = entrada.nextLine();
+        System.out.print("Digite o numero da Conta: ");
+        numero = entrada.nextInt();
+        System.out.print("Digite o valor para deposito: ");
+        saldo = entrada.nextDouble();
+        System.out.printf("%n%nOla %s, obrigado por criar uma conta em nosso banco, sua agencia eh %s, conta %d e seu saldo %.2f ja esta disponivel para saque.", nomeDoCliente, agencia, numero, saldo);
+
+
+    }
+}
+
+
